5a677a7deb37157b82153e37a3c47feed0cdc26f
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2018-09-13 09:49:53 +0200

    Store as a singelton

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Store = require('./lib/Store');
 const Provider = require('./lib/Provider');
+const ArgvProvider = require('./lib/providers/argv.provider');
+const EnvProvidder = require('./lib/providers/env.provider');
+const JsonFileProvider = require('./lib/providers/json-file.provider');
+const JsonProvider = require('./lib/providers/json.provider');
 
 module.exports = {
     Store,
-    Provider
+    Provider,
+    ArgvProvider,
+    EnvProvidder,
+    JsonFileProvider,
+    JsonProvider
 };
