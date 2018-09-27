9c75eff6a24599eb8726177ecf25ff6fd6fc4c52
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2018-09-27 11:49:09 +0200

    Store tests

--- tests/Store.spec.js
+++ tests/Store.spec.js
@@ -5,7 +5,7 @@ const {
     EnvProvidder,
     JsonFileProvider,
     JsonProvider
-} = require('../lib/Store');
+} = require('../index');
 
 test('should return always the same instance', async (t) => {
     const configStore_1 = new Store([]);
@@ -13,13 +13,30 @@ test('should return always the same instance', async (t) => {
     t.is(configStore_1.getSymbol(), configStore_2.getSymbol());
 });
 
+test('should return different instances of store', async (t) => {
+    const configStore_1 = new Store([]);
+    const configStore_2 = new Store([], Symbol());
+
+    t.not(configStore_1.getSymbol(), configStore_2.getSymbol());
+});
+
 test('should load configuration from json provider', async (t) => {
-    const configStore = new ConfigStore([
+    const configStore = new Store([
         new JsonProvider({
             name: 'test',
             p1: 1,
             p2: 2,
             p3: [1,2,3]
         })
-    ]);
+    ], Symbol());
+
+    let config = await configStore.load();
+
+    t.deepEqual(config, {
+        name: 'test',
+        p1: 1,
+        p2: 2,
+        p3: [1,2,3]
+    });
+
 });
