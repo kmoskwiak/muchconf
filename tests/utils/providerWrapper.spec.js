2c08d14fc25cbcc0d3d740f390966c160a6aec18
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2019-01-18 20:28:40 +0100

    wrapper function

---
+++ tests/utils/providerWrapper.spec.js
@@ -0,0 +1,26 @@
+const test = require('ava')
+const wrapper = require('../../lib/utils/providerWrapper');
+
+class MyClass {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    abc() {
+        return [this.a, this.b, this.c];
+    }
+}
+
+test('should call constructor with arguments', async (t) => {
+    const myWrapper = wrapper(MyClass);
+    let wrapped = myWrapper(1,2,3);
+    t.deepEqual([1,2,3], wrapped.abc());
+});
+
+test('should call constructor with arguments', async (t) => {
+    const myWrapper = wrapper(MyClass);
+    let wrapped = myWrapper(1,2);
+    t.deepEqual([1,2, undefined], wrapped.abc());
+});
